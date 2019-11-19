85da6bf5b8c5ce6fe4b5a7757f8a4d7183d1fb06
Jinwoo Park <jinwoopark1673@gmail.com>
2019-11-18 19:35:13 -0800

    Add files via upload

---
+++ shortest_path_algorithm.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+"""
+Return the numpy matrix containing the shortest distance between each vertex.
+Input adjacency_matrix must be the raw String matrix after data_parser!
+"""
+def getShortestDistanceMatrix(adjacency_matrix, num_of_locations):
+    adjacency_matrix = np.array(adjacency_matrix)
+    adjacency_matrix[adjacency_matrix=='x'] = -1
+    adjacency_matrix = adjacency_matrix.astype(np.float)
+    result = np.zeros(adjacency_matrix.shape)
+    for i in range(num_of_locations):
+        dist = result[i]
+        dist = revisedDjikstra(adjacency_matrix, i, dist, num_of_locations)
+        for j in range(num_of_locations):
+            result[i, j] = dist[j]
+            result[j, i] = dist[j]
+    return result
+"""
+Diiiijjjjjikkkksra's algorithm with some known shortest distances.
+"""
+def revisedDjikstra(adjacency_matrix, source, dist, num_of_locations):
+    unVisited = set()
+    for i in range(num_of_locations):
+        if (dist[i] == 0 and i != source):
+            dist[i] = 1000
+        unVisited.add(i)
+    while (len(unVisited) != 0):
+        current = getArgminDist(dist, unVisited)
+        unVisited.remove(current)
+        toVisit = getAdjacentUnvisited(current, adjacency_matrix, unVisited)
+        for vertex in toVisit:
+            cost = dist[current] + adjacency_matrix[current, vertex]
+            if (cost < dist[vertex]):
+                dist[vertex] = cost
+    return dist
+
+def getAdjacentUnvisited(current, adjacency_matrix, unVisited):
+    return [i for i in unVisited if adjacency_matrix[current, i] > 0]
+
+def getArgminDist(dist, unVisited):
+    minValue = 1000
+    minIndex = 0
+    for s in unVisited:
+        minIndex = s
+        break;
+    for vertex in unVisited:
+        if (dist[vertex] < minValue):
+            minValue = dist[vertex]
+            minIndex = vertex
+    return minIndex
